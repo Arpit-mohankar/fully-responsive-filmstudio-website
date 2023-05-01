083acf91bf840de0d0387bbd9101b2ffc1ddb584
Arpit-mohankar <92590594+Arpit-mohankar@users.noreply.github.com>
2023-05-01 16:56:50 +0530

    Update script.js
    added a functionality of when clicked on hamburger menu links the menu is closed

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 $(function() {
-  
     $(".menu-link").click(function(e) {
       e.preventDefault();
       $(".menu").toggleClass("open");
       $(".menu-overlay").toggleClass("open");
     });
-  
   });
+
+  $(document).on("click", "#clix", function(e) {
+    e.preventDefault();
+    var href = $(this).attr("href");
+    $(".menu").removeClass("open");
+    $(".menu-overlay").removeClass("open");
+    window.location.href = href;
+  });
+
   
   
